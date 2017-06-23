6010736be1448fca3e37a01a4dd25a0cc2ba3445
Aldo Contreras <aldocontreras@Aldos-MacBook-Air.local>
2017-06-24 00:02:06 +0200

    Serial apart

--- app.py
+++ app.py
@@ -51,13 +51,20 @@ sensor_args = {
 """
 # Connecting telemetry module
 try:
-    arduino = serial.Serial('/dev/ttyUSB1',baudrate=9600)
-    ser = serial.Serial('/dev/ttyUSB0', 57600)#cambiar por purto serial de pixhawk
-    arduino.open()
+    ser = serial.Serial('/dev/ttyUSB0', 57600) #cambiar por purto serial de pixhawk
     ser.open()
-    print "Conected to:", ser.name, "and ", arduino.name
+    print "Conected to:", ser.name
 except Exception, e:
     print "Serial is not avalible..", e
+
+# Connecting Arduino module
+try:
+    arduino = serial.Serial('/dev/ttyUSB1',baudrate=9600)
+    arduino.open()
+    print "Conected to:", arduino.name
+except Exception, e:
+    print "Serial is not avalible..", e
+
 
 # Connecting Mavproxy
 try:
