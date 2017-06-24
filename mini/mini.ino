06f4723aa7d2cf1d75793607dd3ea88c57c7ced5
Aldo Contreras <aldocontreras@Aldos-MacBook-Air.local>
2017-06-24 09:13:02 +0200

    Better indentation

--- mini/mini.ino
+++ mini/mini.ino
@@ -1,20 +1,20 @@
-
+// Code to get analog data from 0, 1, 2 inputs
+// and sended by serial port
 void setup () {
-      Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop () {
-    if(Serial.available()) {
-            char c = Serial.read();
-         if (c=='B') {
-                  Serial.print(analogRead(0));
-                  Serial.print(",");
-                  Serial.print(analogRead(1));
-                  Serial.print(",");
-                  Serial.print(analogRead(2));
-                        } 
-            }
-            delay(100); 
-            }
-            
+  if(Serial.available()) {
+    char c = Serial.read();
+    if (c=='B') {
+      Serial.print(analogRead(0));
+      Serial.print(",");
+      Serial.print(analogRead(1));
+      Serial.print(",");
+      Serial.print(analogRead(2));
+      }
+  }
+  delay(100);
+}
 
